9aa810ae4fd6134aedd1cff6444f0b9f91b0a968
Mulaudzi-MK <96807491+Mulaudzi-MK@users.noreply.github.com>
2024-10-10 22:34:19 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,27 +10,50 @@ variable "aws_secret_key" {
   default     = ""
 }
 
-variable "aws_region" {
-  description = "AWS region"
+variable "cluster_name" {
+  description = "The name of the EKS cluster"
   type        = string
-  default     = "us-east-1"
+  default     = "my-eks-cluster"
 }
 
-
-variable "ami" {
-   type        = string
-   description = "Ubuntu AMI ID"
-   default     = "ami-0f5ee92e2d63afc18"
+variable "node_group_name" {
+  description = "The name of the EKS node group"
+  type        = string
+  default     = "my-node-group"
 }
 
-variable "instance_type" {
-   type        = string
-   description = "Instance type"
-   default     = "t2.micro"
+variable "desired_capacity" {
+  description = "The desired number of nodes in the node group"
+  type        = number
+  default     = 2
 }
 
-variable "name_tag" {
-   type        = string
-   description = "Name of the EC2 instance"
-   default     = "My EC2 Instance"
+variable "max_capacity" {
+  description = "The maximum number of nodes in the node group"
+  type        = number
+  default     = 3
+}
+
+variable "min_capacity" {
+  description = "The minimum number of nodes in the node group"
+  type        = number
+  default     = 1
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "node_instance_type" {
+  description = "The EC2 instance type for the EKS worker nodes"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "subnet_cidrs" {
+  description = "The CIDR blocks for the subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
