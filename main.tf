69224b6a51359570bd8a65afbfe6d17d7f9c46a3
Mulaudzi-MK <96807491+Mulaudzi-MK@users.noreply.github.com>
2024-10-10 22:19:08 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,38 +1,8 @@
-terraform
-# Configure AWS Provider
-provider "aws" {
-  region = "us-west-2"
-}
+resource "aws_instance" "public_instance" {
+ ami           = var.ami
+ instance_type = var.instance_type
 
-# Create EKS Cluster
-resource "aws_eks_cluster" "example" {
-  name     = "example-cluster"
-  role_arn = aws_iam_role.example.arn
-}
-
-# Create EKS Node Group
-resource "aws_eks_node_group" "example" {
-  cluster_name    = aws_eks_cluster.example.name
-  node_group_name = "example-node-group"
-  instance_types  = ["t2.medium"]
-  node_count      = 3
-}
-
-# Create IAM Role for EKS
-resource "aws_iam_role" "example" {
-  name        = "example-eks-role"
-  description = "EKS Cluster Role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Principal = {
-          Service = "(link unavailable)"
-        }
-        Effect = "Allow"
-      }
-    ]
-  })
+ tags = {
+   Name = var.name_tag,
+ }
 }
