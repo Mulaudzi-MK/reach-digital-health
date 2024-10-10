b5b835de4903e1c81bcf1e9efbbfb97be81d418b
Mulaudzi-MK <96807491+Mulaudzi-MK@users.noreply.github.com>
2024-10-10 15:27:36 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
-# Create EKS Node Group
-resource "aws_eks_node_group" "example" {
-  cluster_name    = aws_eks_cluster.example.name
-  node_group_name = "example-node-group"
-  instance_types  = "t2.medium"
-  node_count      = 3
+resource "aws_instance" "public_instance" {
+ ami           = var.ami
+ instance_type = var.instance_type
+
+ tags = {
+   Name = var.name_tag,
+ }
 }
