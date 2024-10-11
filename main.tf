ea8e6ae098770604eeb032a52f0f604ca1e79aef
Mulaudzi-MK <96807491+Mulaudzi-MK@users.noreply.github.com>
2024-10-11 09:57:33 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -96,15 +96,4 @@ resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
   role       = aws_iam_role.eks_node_role.name
 }
 
-resource "aws_iam_role_policy_attachment" "eks_registry_readonly" {
-  role       = aws_iam_role.eks_node_group_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
 
-resource "aws_eks_node_group" "this" {
-  cluster_name    = aws_eks_cluster.this.name
-  node_group_name = "my-node-group"
-  node_role       = aws_iam_role.eks_node_group_role.arn
-
-  # Other node group settings...
-}
